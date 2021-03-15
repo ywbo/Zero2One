5c45c674ada7ba9c0dfba626136b981ad716433b
Miller <yuwenbo00@163.com>
2021-03-15 17:24:57 +0800

    Role实体类
    Role实体类

---
+++ Wonderful_Code/Vita/Module/system/src/main/java/com/agu/module/system/domain/Role.java
@@ -0,0 +1,86 @@
+package com.agu.module.system.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+/**
+ * Role实体类
+ *
+ * @author yuwenbo
+ * @since 2021-03-15
+ */
+@Data
+@Entity
+@Table(name = "sys_role")
+@ToString(exclude = {"users", "menus", "createBy", "updateBy"})
+@EqualsAndHashCode(exclude = {"users", "menus", "createBy", "updateBy"})
+@EntityListeners(AuditingEntityListener.class)
+@SQLDelete(sql = "UPDATE sys_role" + StatusUtil.SLICE_DELETE)
+@Where(clause = StatusUtil.NOT_DELETE)
+public class Role implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String title;
+    private String remark;
+    @CreatedDate
+    private Date createDate;
+    @LastModifiedDate
+    private Date updateDate;
+    @CreatedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotFound(action = NotFoundAction.IGNORE)
+    @JoinColumn(name = "create_by")
+    @JsonIgnore
+    private User createBy;
+    @LastModifiedBy
+    @ManyToOne(fetch = FetchType.LAZY)
+    @NotFound(action = NotFoundAction.IGNORE)
+    @JoinColumn(name = "update_by")
+    @JsonIgnore
+    private User updateBy;
+    private Byte status = StatusEnum.OK.getCode();
+
+    @ManyToMany(mappedBy = "roles", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JsonIgnore
+    private Set<User> users = new HashSet<>(0);
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "sys_role_menu",
+            joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "menu_id"))
+    @JsonIgnore
+    private Set<Menu> menus = new HashSet<>(0);
+}
