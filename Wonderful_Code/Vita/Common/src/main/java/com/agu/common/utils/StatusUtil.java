49210722cc90d925e304204d4aada980102fbc2b
Miller <yuwenbo00@163.com>
2021-03-15 17:32:44 +0800

    数据状态工具
    数据状态工具

---
+++ Wonderful_Code/Vita/Common/src/main/java/com/agu/common/utils/StatusUtil.java
@@ -0,0 +1,36 @@
+package com.agu.common.utils;
+
+import com.agu.common.constant.StatusConst;
+import com.agu.common.enums.ResultEnum;
+import com.agu.common.enums.StatusEnum;
+import com.agu.common.exception.ResultException;
+
+/**
+ * 数据状态工具
+ *
+ * @author yuwenbo
+ * @since 2021-03-15
+ */
+public class StatusUtil {
+    /**
+     * 逻辑删除语句
+     */
+    public static final String SLICE_DELETE = " set status=" + StatusConst.DELETE + " WHERE id=?";
+
+    /**
+     * 不等于逻辑删除条件语句
+     */
+    public static final String NOT_DELETE = "status != " + StatusConst.DELETE;
+
+    /**
+     * 获取状态StatusEnum对象
+     * @param param 状态字符参数
+     */
+    public static StatusEnum getStatusEnum(String param){
+        try {
+            return StatusEnum.valueOf(param.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new ResultException(ResultEnum.STATUS_ERROR);
+        }
+    }
+}
