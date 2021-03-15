24e7fb010cc7c7b1f47e478516d7a644e7a844cc
Miller <yuwenbo00@163.com>
2021-03-15 17:32:10 +0800

    数据状态常量
    数据状态常量

---
+++ Wonderful_Code/Vita/Common/src/main/java/com/agu/common/constant/StatusConst.java
@@ -0,0 +1,18 @@
+package com.agu.common.constant;
+
+/**
+ * 数据状态常量
+ *
+ * @author yuwenbo
+ * @since 2021-03-15
+ */
+public class StatusConst {
+    /** 正常状态码 */
+    public static final byte OK = 1;
+
+    /** 冻结状态码 */
+    public static final byte FREEZED = 2;
+
+    /** 删除状态码 */
+    public static final byte DELETE = 3;
+}
