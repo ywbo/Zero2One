6fe643259bf8488e62a70f8c694603875867f73c
Miller <yuwenbo00@163.com>
2021-03-17 16:17:03 +0800

    Excel 操作类型
    Excel 操作类型

---
+++ Wonderful_Code/Vita/Component/excel/src/main/java/com/agu/component/excel/enums/ExcelType.java
@@ -0,0 +1,12 @@
+package com.agu.component.excel.enums;
+
+/**
+ * Excel 操作类型
+ *
+ * @author yuwenbo
+ * @since 2021-03-17
+ */
+public enum ExcelType {
+    // ALL：全部操作；IMPORT：导入操作；EXPORT：导出操作
+    ALL, IMPORT, EXPORT;
+}
