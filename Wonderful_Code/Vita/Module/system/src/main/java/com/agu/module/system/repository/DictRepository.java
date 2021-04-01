24b97637373e25c8696b9c529d3333c6bab76d18
Miller <yuwenbo00@163.com>
2021-04-01 16:44:38 +0800

    Create DictRepository.java

---
+++ Wonderful_Code/Vita/Module/system/src/main/java/com/agu/module/system/repository/DictRepository.java
@@ -0,0 +1,27 @@
+package com.agu.module.system.repository;
+
+
+import com.agu.module.system.domain.Dict;
+
+/**
+ * @author yuwenbo
+ * @date 2021-04-01
+ */
+public interface DictRepository extends BaseRepository<Dict, Long> {
+
+    /**
+     * 根据字典标识查询
+     * @param name 字典标识
+     * @param status 状态
+     * @return 字典信息
+     */
+    public Dict findByNameAndStatus(String name, Byte status);
+
+    /**
+     * 根据标识查询字典数据,且排查指定ID的字典
+     * @param name 字典标识
+     * @param id 字典ID
+     * @return 字典信息
+     */
+    public Dict findByNameAndIdNot(String name, Long id);
+}
